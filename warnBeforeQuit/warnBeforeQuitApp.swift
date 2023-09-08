349fe9f0cf2fceee353140b2832c5b5b33c1f368
aanoifjia <aaanoif17041048@gmail.com>
2023-09-08 09:04:28 -0400

    Initial Commit

---
+++ warnBeforeQuit/warnBeforeQuitApp.swift
@@ -0,0 +1,17 @@
+//
+//  warnBeforeQuitApp.swift
+//  warnBeforeQuit
+//
+//  Created by 李佳颖 on 2023/9/8.
+//
+
+import SwiftUI
+
+@main
+struct warnBeforeQuitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
